704976371e9f477f433ba6d98ff335b31992bf96
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-03-23 18:11:12 -0400

    server setupt practice

--- server_lesson_1/server.js
+++ server_lesson_1/server.js
@@ -17,6 +17,8 @@ app.use(cors());
 // Initialize main project folder
 app.use(express.static('demo'));
 
+
+
 const port = 8080;
 
 const server = app.listen(port, listening);
@@ -24,4 +26,40 @@ const server = app.listen(port, listening);
 function listening() {
     console.log('Server is running');
     console.log(`running on localhost ${port}`);
+}
+
+app.get('/', function(req, res) {
+    res.send('All set and running')
+})
+
+// ROUTES nad POST requests
+const data = []
+
+app.post('/addMovie', addMovie)
+
+function addMovie(req, res) {
+    console.log(req.body)
+    data.push(req.body)
+}
+// ------------------------------------------------------------
+/* 1) In the file, server.js, create a POST route that uses the url
+ add and sends the response POST received when used to make a request.
+ */
+
+app.post('/add', callBack);
+
+function callBack(req, res) {
+    res.send('POST received');
+}
+
+/* 2) Add a POST route for adding a favorite animal via the path ’/animal’ 
+to an array named data. You will need to create the array as well.
+ */
+
+const data = [];
+
+app.post('/animal', addAnimal)
+
+function addAnimal(req, res) {
+    data.push(req.body)
 }
